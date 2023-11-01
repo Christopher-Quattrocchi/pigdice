8838582b3b511b047935300f5638274ecc069da4
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-11-01 09:55:03 -0700

    Test 1 passed to give random dice output
    Co-Authored by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,12 @@
+// Business Logic
+
+function rollDice(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
 
-
+function diceRoll() [
+    return Math.floor(Math.random() * (max - min + 1) + min); 
+]
