d45ddb4dddf71580898a8dedec9192c70aa8782b
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-11-01 15:47:15 -0700

    Updated to have roll and hold button and game functions as intended
    Co-Authored by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -9,24 +9,20 @@ function Player(name) {
     this.totalScore = 0;
     this.roundScore = 0;
     this.playerTurn = false;
+    this.playerVictory = false;
 }
 
 
 //get random number 1-6, add to roundscore if not 1
 Player.prototype.diceRoll = function () {
+    displayRoll = document.getElementById("roll")
     let roll = Math.floor(Math.random() * 6) + 1;
     if (roll === 1) {
+        displayRoll.innerText = "You rolled a: " + roll;
         this.roundScore = 0;
-        if (playerOne.playerTurn) {
-            playerOne.playerTurn = false;
-            playerTwo.playerTurn = true;
-            displayTurn();
-        } else if (playerTwo.playerTurn) {
-            playerOne.playerTurn = true;
-            playerTwo.playerTurn = false;
-            displayTurn();
-        }
+        changePlayer();
     } else {
+        displayRoll.innerText  = "You rolled a: " + roll;
         this.roundScore += roll;
     }
 }
@@ -35,51 +31,24 @@ Player.prototype.diceRoll = function () {
 Player.prototype.updateScore = function (roundScore) {
     this.totalScore = this.totalScore + this.roundScore;
     this.roundScore = 0;
-    if (playerOne.playerTurn) {
-        playerOne.playerTurn = false;
-        playerTwo.playerTurn = true;
-        displayTurn();
-    } else if (playerTwo.playerTurn) {
-        playerOne.playerTurn = true;
-        playerTwo.playerTurn = false;
-        displayTurn();
-    }
+    changePlayer();
 }
 
-// function diceRoll() {//Uses math functions to simulate rolling a die
-//     let roll = Math.floor(Math.random() * 6) + 1;
-//     if (roll === 1) {
-//         roundScore = 0;
-//     } else {
-//         roundScore += roll;
-//     }
-// }
-
-
-
-
-// Player.prototype.addRoundScore = function (roundScore) {
-//     this.score = this.score + roundScore;
-// };
-
 function changePlayer() {
-    if (playerTurn === false) {
-        playerTurn = true;
-    } else if (playerTurn === true) {
-        playerTurn = false
-    }
-    let roundScore = 0;
-    let myResult = playerTurn;
-    return myResult;
-
+    playerOne.playerTurn = !playerOne.playerTurn;
+    playerTwo.playerTurn = !playerTwo.playerTurn;
+    displayTurn();
 }
 
 // UI Logic
 // myPlayer = new Player("NameOne", 0);//For testing
 
 window.addEventListener("load", function () {
+    const holdbutton = document.getElementById("holdbutton");
+    const rollbutton = document.getElementById("rollbutton");
     document.getElementById("create-player").addEventListener("submit", createPlayer);
-    document.getElementById("pig-dice").addEventListener("submit", handleSubmission);
+    document.getElementById("pig-dice").addEventListener("click", handleSubmission);
+  
 })
 
 let playerOne; //Defining a player, setting to null
@@ -88,24 +57,26 @@ let playerTwo;
 
 function createPlayer(e) {
     e.preventDefault();
+    form = document.getElementById("pig-dice");
     const name = document.getElementById("name").value;
     const pTurn = document.getElementById("turn");
     const hideForm = document.getElementById("create-player");
     const p1 = document.getElementById("p1");
     const p2 = document.getElementById("p2");
+    form.setAttribute("class", "not-hidden");
     if (!playerOne) {
         playerOne = new Player(name);
         playerOne.playerTurn = true;
-        p1.innerText = name;
+        p1.innerText = "Player one: " + name;
     } else if (!playerTwo) {
         playerTwo = new Player(name);
-        p2.innerText = name;
+        p2.innerText = "Player two: " + name;
         hideForm.setAttribute("class", "hidden");
     } else {
-        
+
         console.log("Stop breaking stuff");
     }
-   
+
     if (playerOne.playerTurn === true) {
         pTurn.innerText = "It is " + playerOne.name + "'s turn";
     } else if (playerTwo.playerTurn === true) {
@@ -115,38 +86,53 @@ function createPlayer(e) {
 
 function handleSubmission(e) {
     e.preventDefault();
-    const holdOrRoll = document.getElementById("rollOrHold").value;
+    const hideMe = document.getElementById("hideme");
+    const holdbutton = document.getElementById("holdbutton");
+    const rollbutton = document.getElementById("rollbutton");
     const pTotal1 = document.getElementById("total-score-p1");
     const pRound1 = document.getElementById("round-score-p1");
     const pTotal2 = document.getElementById("total-score-p2");
     const pRound2 = document.getElementById("round-score-p2");
+    const victory = document.getElementById("victory");
 
-    const pTurn = document.getElementById("turn");
+
+
     if (playerOne.playerTurn === true) {
         // pTurn.innerText = "It is " + playerOne.name + "'s turn";
-        if (holdOrRoll === "roll") {
+        if (e.target === rollbutton) {
             playerOne.diceRoll();
             console.log(playerOne.name, " clicked roll. Your total score is: ", playerOne.totalScore, "and your round score is: ", playerOne.roundScore);
             pRound1.innerText = "Round Score: " + playerOne.roundScore;
             pTotal1.innerText = "Total Score: " + playerOne.totalScore;
-        } else {
+        } else if (e.target === holdbutton) {
             playerOne.updateScore();
             console.log(playerOne.name, " clicked hold. Your total score is: ", playerOne.totalScore, "and your round score is: ", playerOne.roundScore);
             pRound1.innerText = "Round Score: " + playerOne.roundScore;
             pTotal1.innerText = "Total Score: " + playerOne.totalScore;
+            playerOne.victoryCheck()
+            if (playerOne.playerVictory) {
+                victory.innerText = playerOne.name + " Victory! Winner Winner Chicken Dinner!!";
+                hideMe.setAttribute("class", "hidden");
+
+            }
         }
     } else if (playerTwo.playerTurn === true) {
         // pTurn.innerText = "It is " + playerTwo.name + "'s turn";
-        if (holdOrRoll === "roll") {
+        if (e.target === rollbutton) {
             playerTwo.diceRoll();
             console.log(playerTwo.name, " clicked roll. Your total score is: ", playerTwo.totalScore, "and your round score is: ", playerTwo.roundScore);
             pRound2.innerText = "Round Score: " + playerTwo.roundScore;
             pTotal2.innerText = "Total Score: " + playerTwo.totalScore;
-        } else {
+        } else if (e.target === holdbutton) {
             playerTwo.updateScore();
             console.log(playerTwo.name, " clicked hold. Your total score is: ", playerTwo.totalScore, "and your round score is: ", playerTwo.roundScore);
             pRound2.innerText = "Round Score: " + playerTwo.roundScore;
             pTotal2.innerText = "Total Score: " + playerTwo.totalScore;
+            playerTwo.victoryCheck();
+            if (playerTwo.playerVictory) {
+                victory.innerText = playerTwo.name + " Victory! Winner Winner Chicken Dinner!!";
+                hideMe.setAttribute("class", "hidden");
+            }
         }
     }
 }
@@ -160,17 +146,10 @@ function displayTurn() {
     }
 }
 
-// function handlePlayerTurn(player, )
-
-//REFERENCE
-
-// let count = 0;
-
-// while (count < 5) {
-//   console.log("Count: " + count);
-//   count++; // Increment the count in each iteration
-// }
-// let roundScore = 0;
-// while (dieRoll !== 1 && holdScore !== true) {
-//     roundScore += dieRoll;
-// }
+Player.prototype.victoryCheck = function () {
+    if (this.totalScore >= 100) {
+        this.playerVictory = true;
+    } else {
+        playerVictory = false;
+    }
+}
