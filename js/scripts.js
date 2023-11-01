0ae62651dd5e0e82e3c9cd37ce51f8207695955e
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-11-01 10:14:20 -0700

    Passed second test to add player with inital score of 0
    Co-Authored by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,14 @@
 // Business Logic
 
-function diceRoll() {
+function diceRoll() {//Uses math functions to simulate rolling a die
     return Math.floor(Math.random() * 6) + 1;
 }
 
+function Player(name, score) {
+    this.name = name;
+    this.score = 0;
+}
+
+Player.prototype.addScore(roundScore) {
+    this.score += roundScore;
+}
