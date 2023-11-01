0ab2d3710b14c62e7ffbd649769e0c481d447384
Ravin Fisher <134343711+Ravinfish@users.noreply.github.com>
2023-11-01 11:59:05 -0700

    Roll dice function works to add roll score to total score and will hold score when hold is selected
    Co-Authored by: Christopher Quattrocchi <chrisquattrocchi@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -1,53 +1,70 @@
 // Business Logic
 
-Player.prototype.diceRoll()
+// let totalScore = 0;//made global
+// let roundScore = 0;
 
-function diceRoll() {//Uses math functions to simulate rolling a die
-  return Math.floor(Math.random() * 6) + 1;
+//Create player object
+function Player(name) {
+    this.name = name;
+    this.totalScore = 0;
+    this.roundScore = 0;
+    this.playerTurn = true;
 }
 
-function Player(name, score, roundScore) {
-  this.name = name;
-  this.score = 0;
-  this.roundScore = 0;
-}
 
-Player.prototype.roundScore(dieRoll) {
-
-}
-
-Player.prototype.addRoundScore = function(roundScore) {
-  this.score = this.score + roundScore;
-};
-
-function changePlayer () {
-  if (playerTurn === false) {
-    playerTurn = true;
-} else if (playerTurn === true) {
-  playerTurn = false
-}
-let roundScore = 0;
-let myResult = playerTurn;
-return myResult;
-  
-}
-
-function roundScore() {
-    let roundScore = 0;
-    if (dieNumber !== 1) {
-        roundScore += dieNumber;
+//get random number 1-6, add to roundscore if not 1
+Player.prototype.diceRoll = function () {
+    let roll = Math.floor(Math.random() * 6) + 1;
+    if (roll === 1) {
+        // this.roundScore = 0;
+        this.playerTurn = false;
     } else {
-        roundScore = 0;
+        this.roundScore += roll;
     }
 }
 
+//add roundscore to total score
+Player.prototype.updateScore = function (roundScore) {
+    this.totalScore = this.totalScore + this.roundScore;
+    this.roundScore = 0;
+}
+
+// function diceRoll() {//Uses math functions to simulate rolling a die
+//     let roll = Math.floor(Math.random() * 6) + 1;
+//     if (roll === 1) {
+//         roundScore = 0;
+//     } else {
+//         roundScore += roll;
+//     }
+// }
+
+
+
+
+// Player.prototype.addRoundScore = function (roundScore) {
+//     this.score = this.score + roundScore;
+// };
+
+function changePlayer() {
+    if (playerTurn === false) {
+        playerTurn = true;
+    } else if (playerTurn === true) {
+        playerTurn = false
+    }
+    let roundScore = 0;
+    let myResult = playerTurn;
+    return myResult;
+
+}
 
 // UI Logic
-myPlayer = new Player("NameOne", 0);//For testing
+// myPlayer = new Player("NameOne", 0);//For testing
 
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
     this.document.getElementById("pig-dice").addEventListener("submit", handleSubmission);
 })
+
+let playerOne = null; //Defining a player, setting to null
 
 function handleSubmission(e) {
     e.preventDefault();
@@ -55,14 +72,19 @@ function handleSubmission(e) {
     const name = document.getElementById("name").value;
     const pTotal = document.getElementById("total-score");
     const pRound = document.getElementById("round-score");
+
+    if (!playerOne) {//Create a new player IF there isn't a player
+        playerOne = new Player(name);
+    }
+
     if (holdOrRoll === "roll") {
-        let roll = diceRoll();
-        let roundScore = roundScore(roll);
-        addRoundScore(roundScore);
-        
+        playerOne.diceRoll();
+        console.log(playerOne.name, " clicked roll. Your total score is: ", playerOne.totalScore, "and your round score is: ", playerOne.roundScore);
+    } else {
+        playerOne.updateScore();
+        console.log(playerOne.name, " clicked hold. Your total score is: ", playerOne.totalScore, "and your round score is: ", playerOne.roundScore);
     }
 }
-
 
 //REFERENCE
 
