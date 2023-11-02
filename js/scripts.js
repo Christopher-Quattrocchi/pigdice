f708045ca3326531ec207adf90e4db21ac2c51c5
Christopher Quattrocchi <chrisquattrocchi@gmail.com>
2023-11-02 11:33:48 -0700

    computer strategy has been remade into an async function, this fixed many issues

--- js/scripts.js
+++ js/scripts.js
@@ -10,16 +10,48 @@ function Player(name) {
 }
 
 //get random number 1-6, add to roundscore if not 1
-Player.prototype.diceRoll = function () {
-    const displayRoll = document.getElementById("roll")
-    let roll = Math.floor(Math.random() * 6) + 1;
-    if (roll === 1) {
-        displayRoll.innerText = "You rolled a: " + roll;
+Player.prototype.diceRoll = async function () {
+    const displayRoll1 = document.getElementById("roll1")
+    const displayRoll2 = document.getElementById("roll2")
+    const pTotal1 = document.getElementById("total-score-p1");
+    const pRound1 = document.getElementById("round-score-p1");
+    const pTotal2 = document.getElementById("total-score-p2");
+    const pRound2 = document.getElementById("round-score-p2");
+
+    let roll1 = Math.floor(Math.random() * 6) + 1;
+    let roll2 = Math.floor(Math.random() * 6) + 1;
+
+    if (roll1 === 1 && roll2 === 1) {
         this.roundScore = 0;
+        this.totalScore = 0;
+
+        displayRoll1.innerText = "You rolled a: " + roll1;
+        displayRoll2.innerText = "You rolled a: " + roll2;
+        updateDisplay(this, pTotal1, pRound1, pTotal2, pRound2);
+        await delay(1000);
+        changePlayer();
+    } else if (roll1 === 1 || roll2 === 1) {
+        displayRoll1.innerText = "You rolled a: " + roll1;
+        displayRoll2.innerText = "You rolled a: " + roll2;
+        this.roundScore = 0;
+        updateDisplay(this, pTotal1, pRound1, pTotal2, pRound2);
+        await delay(1000);
         changePlayer();
     } else {
-        displayRoll.innerText = "You rolled a: " + roll;
-        this.roundScore += roll;
+        displayRoll1.innerText = "You rolled a: " + roll1;
+        displayRoll2.innerText = "You rolled a: " + roll2;
+        this.roundScore += roll1 + roll2;
+        updateDisplay(this, pTotal1, pRound1, pTotal2, pRound2);
+    }
+}
+
+function updateDisplay(player, pTotal1, pRound1, pTotal2, pRound2) {
+    if (playerOne === player) {
+        pRound1.innerText = "Round Score: " + player.roundScore;
+        pTotal1.innerText = "Total Score: " + player.totalScore;
+    } else if (playerTwo === player || computerOpponent === player) {
+        pRound2.innerText = "Round Score: " + player.roundScore;
+        pTotal2.innerText = "Total Score: " + player.totalScore;
     }
 }
 
@@ -27,53 +59,103 @@ Player.prototype.diceRoll = function () {
 Player.prototype.updateScore = function () {
     this.totalScore = this.totalScore + this.roundScore;
     this.roundScore = 0;
-    changePlayer();
 }
 
 function changePlayer() {
     playerOne.playerTurn = !playerOne.playerTurn;
+    displayTurn();
     if (playerTwo) {
         playerTwo.playerTurn = !playerTwo.playerTurn;
-    }
-    if (computerOpponent) {
+        displayTurn();
+    } else if (computerOpponent) {
         computerOpponent.playerTurn = !computerOpponent.playerTurn;
-    } computerStrategy();
+        computerStrategy();
+        console.log("computerStrategy called in changePlayer");
+    }
     displayTurn();
 }
 
+//reference
+function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
 
-function computerStrategy() {
+// async function xyz() {
+//     //... (initialization code)
+
+//     while (computerOpponent.playerTurn) {
+//         //... (game logic)
+//         await delay(1000);
+//     }
+// }
+
+
+async function computerStrategy() {
     if (!computerOpponent.playerTurn) return;
-
+    console.log("In computerStrategy");
+    const pTotal1 = document.getElementById("total-score-p1");
     const pTotal2 = document.getElementById("total-score-p2");
+    const pRound1 = document.getElementById("round-score-p1");
     const pRound2 = document.getElementById("round-score-p2");
     const victory = document.getElementById("victory");
     const hideMe = document.getElementById("hideme");
 
-    const interval = setInterval(() => {
-        if (!computerOpponent.playerTurn) {
-            clearInterval(interval);
-            return;
+    while (computerOpponent.playerTurn && computerOpponent.totalScore < 100 && computerOpponent.roundScore < 22) {
+        console.log("In while loop");
+        await computerOpponent.diceRoll();
+        updateDisplay(computerOpponent, pTotal1, pRound1, pTotal2, pRound2);
+        await delay(1000);
+        console.log("after first delay");
+    }
+    if (computerOpponent.roundScore >= 22) {
+        console.log("inside conditional that checks for comp score equal or over 22");
+        computerOpponent.updateScore();
+        updateDisplay(computerOpponent, pTotal1, pRound1, pTotal2, pRound2);
+        console.log("computer held, screen should be updated");
+        await delay(1000);
+        computerOpponent.victoryCheck();
+        console.log("victory check ran");
+        if (computerOpponent.playerVictory) {
+            victory.innerText = computerOpponent.name + " Victory! Winner Winner Chicken Dinner!!";
+            hideMe.setAttribute("class", "hidden");
+        } else {
+            changePlayer();
+            console.log("player change");
         }
-
-        computerOpponent.diceRoll();
-        pRound2.innerText = "Round Score: " + computerOpponent.roundScore;
-        pTotal2.innerText = "Total Score: " + computerOpponent.totalScore;
-
-        if (computerOpponent.roundScore > 12 || computerOpponent.totalScore >= 100) {
-            clearInterval(interval);
-            computerOpponent.updateScore();
-            computerOpponent.victoryCheck();
-            if (computerOpponent.playerVictory) {
-                victory.innerText = computerOpponent.name + " Victory! Winner Winner Chicken Dinner!!";
-                hideMe.setAttribute("class", "hidden");
-            } else {
-                changePlayer();
-            }
-        }
-    }, 1000);
+    }
+    console.log("end of computerStrategy");
 }
 
+// function computerStrategy() {
+//     if (!computerOpponent.playerTurn) return;
+
+//     const pTotal2 = document.getElementById("total-score-p2");
+//     const pRound2 = document.getElementById("round-score-p2");
+//     const victory = document.getElementById("victory");
+//     const hideMe = document.getElementById("hideme");
+
+//     const interval = setInterval(() => {
+//         if (!computerOpponent.playerTurn || computerOpponent.roundScore > 22) {
+//             // computerOpponent.updateScore();
+//             updateDisplay(computerOpponent, pTotal1, pRound1, pTotal2, pRound2);
+//             clearInterval(interval);
+//             if (computerOpponent.roundScore > 22 || computerOpponent.totalScore >= 100) {
+//                 computerOpponent.updateScore();
+//                 updateDisplay(computerOpponent, pTotal1, pRound1, pTotal2, pRound2);
+//                 computerOpponent.victoryCheck();
+//                 if (computerOpponent.playerVictory) {
+//                     victory.innerText = computerOpponent.name + " Victory! Winner Winner Chicken Dinner!!";
+//                     hideMe.setAttribute("class", "hidden");
+//                 } else {
+//                     changePlayer();
+//                 }
+//             }
+//             return;
+//         }
+//         computerOpponent.diceRoll();
+//         updateDisplay(computerOpponent, pTotal1, pRound1, pTotal2, pRound2);
+//     }, 1000);
+// }
 
 // UI Logic
 
@@ -96,7 +178,6 @@ function startWithComputer(e) {
     const p2 = document.getElementById("p2");
     displayTurn();
     p2.innerText = "Player Two: " + computerOpponent.name;
-
 }
 
 function createPlayer(e) {
@@ -113,10 +194,10 @@ function createPlayer(e) {
         playerOne.playerTurn = true;
         document.getElementById("computer-player").setAttribute("class", "hideme");
         document.getElementById("create-button").innerText = "Create Player 2";
-        p1.innerText = "Player one: " + name;
+        p1.innerText = "Player one: " + playerOne.name;
     } else if (!playerTwo) {
         playerTwo = new Player(name);
-        p2.innerText = "Player two: " + name;
+        p2.innerText = "Player two: " + playerTwo.name;
         hideForm.setAttribute("class", "hidden");
         document.getElementById("computer-player").setAttribute("class", "hidden");
     } else {
@@ -129,7 +210,7 @@ function createPlayer(e) {
     }
 }
 
-function handleSubmission(e) {
+async function handleSubmission(e) {
     e.preventDefault();
     const hideMe = document.getElementById("hideme");
     const holdbutton = document.getElementById("holdbutton");
@@ -142,40 +223,35 @@ function handleSubmission(e) {
 
     if (playerOne.playerTurn === true) {
         if (e.target === rollbutton) {
-            playerOne.diceRoll();
-            console.log(playerOne.name, " clicked roll. Your total score is: ", playerOne.totalScore, "and your round score is: ", playerOne.roundScore);
-            pRound1.innerText = "Round Score: " + playerOne.roundScore;
-            pTotal1.innerText = "Total Score: " + playerOne.totalScore;
+            await playerOne.diceRoll();
         } else if (e.target === holdbutton) {
             playerOne.updateScore();
-            console.log(playerOne.name, " clicked hold. Your total score is: ", playerOne.totalScore, "and your round score is: ", playerOne.roundScore);
-            pRound1.innerText = "Round Score: " + playerOne.roundScore;
-            pTotal1.innerText = "Total Score: " + playerOne.totalScore;
+            updateDisplay(playerOne, pTotal1, pRound1, pTotal2, pRound2);
             playerOne.victoryCheck()
             if (playerOne.playerVictory) {
                 victory.innerText = playerOne.name + " Victory! Winner Winner Chicken Dinner!!";
                 hideMe.setAttribute("class", "hidden");
+            } else {
+                changePlayer();
             }
         }
     } else if (playerTwo && playerTwo.playerTurn === true) {
         if (e.target === rollbutton) {
-            playerTwo.diceRoll();
-            console.log(playerTwo.name, " clicked roll. Your total score is: ", playerTwo.totalScore, "and your round score is: ", playerTwo.roundScore);
-            pRound2.innerText = "Round Score: " + playerTwo.roundScore;
-            pTotal2.innerText = "Total Score: " + playerTwo.totalScore;
+            await playerTwo.diceRoll();
         } else if (e.target === holdbutton) {
             playerTwo.updateScore();
-            console.log(playerTwo.name, " clicked hold. Your total score is: ", playerTwo.totalScore, "and your round score is: ", playerTwo.roundScore);
-            pRound2.innerText = "Round Score: " + playerTwo.roundScore;
-            pTotal2.innerText = "Total Score: " + playerTwo.totalScore;
+            updateDisplay(playerTwo, pTotal1, pRound1, pTotal2, pRound2);
             playerTwo.victoryCheck();
             if (playerTwo.playerVictory) {
                 victory.innerText = playerTwo.name + " Victory! Winner Winner Chicken Dinner!!";
                 hideMe.setAttribute("class", "hidden");
+            } else {
+                changePlayer();
             }
         }
     } else if (computerOpponent && computerOpponent.playerTurn) {
         computerStrategy();
+        console.log("computerStrategy called in handleSubmission");
     }
 }
 
@@ -197,3 +273,4 @@ Player.prototype.victoryCheck = function () {
         this.playerVictory = false;
     }
 }
+
